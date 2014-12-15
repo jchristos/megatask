75a39c0564d2ff271fdab5f01b3887931035ccb0
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-15 11:40:27 -0500

    Store tasks in local storage

--- js/megatask.js
+++ js/megatask.js
@@ -2,12 +2,25 @@ var Megatask = function() {
   function Megatask() {
     this.tasks = [];
     var self = this;
+    var supportsStorage = function() {
+      try {
+        return 'localStorage' in window && window['localStorage'] != null;
+      } catch(e) {
+        return false;
+      }
+    };
     var addTask = function(taskName) {
         self.tasks.push(taskName);
         $('#tasks').append('<li class="list-group-item">' + taskName + '</li>');
+        saveTasks();
+    };
+    var saveTasks = function() {
+      if (supportsStorage()) {
+        localStorage.tasks = JSON.stringify(self.tasks);
+      }
     };
 
-    $('#new_task').submit(function(ev) {  
+    $('#new_task').submit(function(ev) {
       ev.preventDefault();
       var field = $(this.elements.task_name);
       addTask(field.val());
